10b10ec7d6d919da89fa80a763b2c71277870dec
frosedapri <50383498+frosedapri@users.noreply.github.com>
2020-06-17 23:00:10 +0200

    test3

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-# prce_fider
-=======
 # prcie_finder
->>>>>>> bc06bee160450ff614ad22ed5c55ae6453bfc880
--- read.py
+++ read.py
@@ -3,3 +3,4 @@ with open(r"C:\Users\owen\Desktop\Projet Python\website\read_it.txt") as f:
 
 content = [x.strip() for x in content] 
 print(content)
+print("hello")
